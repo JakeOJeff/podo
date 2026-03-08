b807e08c82716c7e3fd9ec8e0efe662b286bca28
Paulyn <jakeojeff@gmail.com>
2026-03-08 06:00:33 +0530

    Working timer System

--- main.lua
+++ main.lua
@@ -4,4 +4,18 @@
 TIMER = 0
 STATUS = "PAUSED"
 MODE = "POMO" -- [ POMO, SBR, LBR ]
+TIMERS  = {
+    ["POMO"] = 25 * 60,
+    ["SBR"] = 5 * 60,
+    ["LBR"] = 10 * 60
+}
 
+
+
+function love.update()
+    
+    if STATUS ~= "PAUSED" and TIMER > 0 then
+        TIMER = math.max(0, TIMER - 1 * dt)
+    end
+
+end
