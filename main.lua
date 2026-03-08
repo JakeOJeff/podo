d591bc03fad74d73efeaf9175d464462b7e41d74
Paulyn <jakeojeff@gmail.com>
2026-03-08 06:08:23 +0530

    Added working String formats and display for timer

--- main.lua
+++ main.lua
@@ -12,10 +12,32 @@ TIMERS  = {
 
 
 
-function love.update()
+LG = love.graphics
+
+
+function love.load()
+    TIMER = TIMERS[MODE]
+end
+
+
+function love.update(dt)
     
     if STATUS ~= "PAUSED" and TIMER > 0 then
         TIMER = math.max(0, TIMER - 1 * dt)
     end
 
+end
+
+function love.keypressed(key)
+    if key == "space" then
+        if STATUS == "PAUSED" then
+            STATUS = "STARTED"
+        else
+            STATUS = "PAUSED"
+        end
+    end
+end
+
+function love.draw()
+    LG.print(string.format("%02d:%02d", math.floor(TIMER/60), math.floor(TIMER%60)))
 end
