70152976479e0f675d8f1f53981dcba8ab1500a0
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-03-11 06:12:45 +0530

    Added working minimum width and height with scaling data

---
+++ conf.lua
@@ -0,0 +1,7 @@
+function love.conf(t)
+    t.window.width = 800         -- Initial window width
+    t.window.height = 600        -- Initial window height
+    t.window.minwidth = 400      -- Minimum window width
+    t.window.minheight = 400     -- Minimum window height
+    t.window.resizable = true    -- Allows the window to be resized by the user
+end
--- main.lua
+++ main.lua
@@ -1,33 +1,32 @@
-LG     = love.graphics
-
--- Declaring Global variables
-TIMER  = 0
-STATUS = "PAUSED"
-MODE   = "POMO" -- [ POMO, SBR, LBR ]
-TIMERS = {
-    ["POMO"] = 25 * 60,
-    ["SBR"] = 5 * 60,
-    ["LBR"] = 10 * 60
-}
-
-wW, wH = love.graphics.getDimensions()
-
-fonts = {
-    xH = love.graphics.newFont("fonts/StardosStencil-Bold.ttf", 55),
-    h = love.graphics.newFont("fonts/StardosStencil-Bold.ttf", 40),
-    m = love.graphics.newFont("fonts/StardosStencil-Bold.ttf", 25),
-    s = love.graphics.newFont("fonts/StardosStencil-Bold.ttf", 10),
-
-}
-
-Button = require "ui.button"
-
-
 function love.load()
-    TIMER = TIMERS[MODE]
+    LG     = love.graphics
 
-    Butt = Button.new(SwitchStatus, 200, 400, "Image", "play.png", fonts.m, 20, 200)
-    Butt.x = wW/2 - Butt.w/2
+    -- Declaring Global variables
+    TIMER  = 0
+    STATUS = "PAUSED"
+    MODE   = "POMO" -- [ POMO, SBR, LBR ]
+    TIMERS = {
+        ["POMO"] = 25 * 60,
+        ["SBR"] = 5 * 60,
+        ["LBR"] = 10 * 60
+    }
+
+    wW, wH = love.graphics.getDimensions()
+
+    fonts  = {
+        xH = love.graphics.newFont("fonts/StardosStencil-Bold.ttf", 55),
+        h = love.graphics.newFont("fonts/StardosStencil-Bold.ttf", 40),
+        m = love.graphics.newFont("fonts/StardosStencil-Bold.ttf", 25),
+        s = love.graphics.newFont("fonts/StardosStencil-Bold.ttf", 10),
+
+    }
+
+    Button = require "ui.button"
+    TIMER  = TIMERS[MODE]
+
+    Butt   = Button.new(SwitchStatus, 200, 300, "Image", "play.png", fonts.m, 20, 200)
+    Butt.x = wW / 2 - Butt.w / 2
+    Butt.y = wH / 1.3 - Butt.h / 2
 end
 
 function love.update(dt)
@@ -49,7 +48,6 @@ function love.mousepressed(x, y, button)
 end
 
 function love.draw()
-
     local displayBox = {
 
     }
@@ -57,7 +55,7 @@ function love.draw()
 
     LG.setFont(fonts.xH)
     local time = string.format("%02d:%02d", math.floor(TIMER / 60), math.floor(TIMER % 60))
-    LG.print(time, wW/2 - fonts.xH:getWidth(time)/2 , 300)
+    LG.print(time, wW / 2 - fonts.xH:getWidth(time) / 2, 200)
 
     DrawButtons()
 end
@@ -72,3 +70,9 @@ function SwitchStatus()
     end
 end
 
+function love.resize(w, h)
+    wW = w
+    wH = h
+    ActiveButtons = {}
+    love.load()
+end
