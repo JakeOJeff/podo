23eb7e6a4bd998cdc548dbc7ef97e7bc3b2e87a8
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-03-12 23:31:16 +0530

    Adding Table insertion of arrays for square objects

--- effects.lua
+++ effects.lua
@@ -2,18 +2,6 @@ local effects = {}
 
 local tween = require "libs.tween"
 
-function effects:load()
-    self.squares = {}
-
-    self.square_width = displayBox.w/20
-    self.square_rows = 20
-    self.square_height = displayBox.h/20
-    self.square_columns = 20
-
-    self.duration = 2
-
-end
-
 local function get_keys(tbl)
     local keys, len = {}, 0
 
@@ -31,13 +19,36 @@ local function pad(x, min, max)
     return math.max(math.min(x, max), min)
 end
 
-local function get_ball_target(column, row)
+local function getSquareTarget(column, row)
     local x = wW/2 - effects.square_width * effects.square_columns + effects.square_width * column * 2
     local y = wH/2 - effects.square_height * effects.square_rows + effects.square_height * row * 2
 
     return {x=x, y=y, radius = effects.square_width}
-    
+
 end
+
+function effects:load()
+    self.squares = {}
+
+    self.square_width = displayBox.w/20
+    self.square_rows = 20
+    self.square_height = displayBox.h/20
+    self.square_columns = 20
+
+    self.duration = 2
+
+
+    for i = 1, self.square_columns do
+        for j = 1, self.square_rows do
+            table.insert(self.squares, {
+                x = (i - 1) * self.square_rows,
+                y = (j - 1) * self.square_columns
+            })
+        end
+    end
+
+end
+
 function effects:update(dt)
     
 end
