5df8f671228fb183f2cd3cf3b803aa3299d742b3
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-03-12 23:26:03 +0530

    Working pad and target balls

--- effects.lua
+++ effects.lua
@@ -1,5 +1,6 @@
 local effects = {}
 
+local tween = require "libs.tween"
 
 function effects:load()
     self.squares = {}
@@ -24,6 +25,19 @@ local function get_keys(tbl)
     return keys
 end
 
+local easing_keys = get_keys(tween.easing)
+
+local function pad(x, min, max)
+    return math.max(math.min(x, max), min)
+end
+
+local function get_ball_target(column, row)
+    local x = wW/2 - effects.square_width * effects.square_columns + effects.square_width * column * 2
+    local y = wH/2 - effects.square_height * effects.square_rows + effects.square_height * row * 2
+
+    return {x=x, y=y, radius = effects.square_width}
+    
+end
 function effects:update(dt)
     
 end
