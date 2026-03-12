d0f89355c5f76db9193a00966bd9c44ffe315f14
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-03-12 23:14:22 +0530

    Get Keys function

--- effects.lua
+++ effects.lua
@@ -8,6 +8,20 @@ function effects:load()
     self.square_rows = 20
     self.square_height = displayBox.h/20
     self.square_columns = 20
+
+    self.duration = 2
+
+end
+
+local function get_keys(tbl)
+    local keys, len = {}, 0
+
+    for k in pairs(tbl) do
+        len = len + 1
+        keys[len] = k
+    end
+
+    return keys
 end
 
 function effects:update(dt)
