aa766bad272bab0172bd928184acf248ce7d2d9e
Paulyn <87922888+JakeOJeff@users.noreply.github.com>
2026-03-13 00:19:40 +0530

    Added tween for hte square

--- effects.lua
+++ effects.lua
@@ -11,19 +11,23 @@ function effects:load()
     self.square_height = displayBox.h/20
     self.square_columns = 20
 
-    self.duration = 2
 
-
-    for i = 1, self.square_columns do
-        for j = 1, self.square_rows do
-            table.insert(self.squares, {
+    for i = 1, self.square_rows do
+        for j = 1, self.square_columns do
+            local a = {
                 x = 0,
                 y = 0,
                 sX = love.math.random(0, wW),
                 sY = love.math.random(0, wH),
                 tX = (i - 1) * self.square_rows,
-                tY = (j - 1) * self.square_columns
-            })
+                tY = (j - 1) * self.square_columns,
+                duration = (i * j)/(self.square_rows * self.square_columns),
+            }
+            a.x = a.sX
+            a.y = a.sY
+            a.tween = tween.new(a.duration, a, {x = a.tX, y = a.tY})
+            
+            table.insert(self.squares, a)
         end
     end
 
